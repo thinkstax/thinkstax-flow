167ddb1529ffcab7d084653904799d8c8608cbcd
Markus Daehn <markus.daehn@gmail.com>
2018-08-30 06:42:59 -0700

    Update type.js

--- src/utils/type.js
+++ src/utils/type.js
@@ -1,27 +1,3 @@
-function isString(value) {
-  return typeof value === 'string';
-}
-
-function isFunction(value) {
-  return typeof value === 'function';
-}
-
-function isObject(value) {
-  return typeof value === 'object';
-}
-
-function isNotString(value) {
-  return !isString(value);
-}
-
-function isNotFunction(value) {
-  return !isFunction(value);
-}
-
-function isNotObject(value) {
-  return !isObject(value);
-}
-
 function isArray(value) {
   return Array.isArray(value);
 }
@@ -30,25 +6,111 @@ function isNotArray(value) {
   return !isArray(value);
 }
 
+function isEmptyArray(value) {
+  return isArray(value) && value.length === 0;
+}
+
+function isNotEmptyArray(value) {
+  return !isEmptyArray(value);
+}
+
+function isFunction(value) {
+  return typeof value === 'function';
+}
+
+function isString(value) {
+  return typeof value === 'string';
+}
+
+function isNotString(value) {
+  return !isString(value);
+}
+
+function isNull(value) {
+  return value === null;
+}
+
+function isNotNull(value) {
+  return !isNull(value);
+}
+
+function isUndefined(value) {
+  return typeof value === 'undefined';
+}
+
+function isObject(value) {
+  return (typeof value === 'object' && isNotNull(value) && isNotArray(value));
+}
+
+function isNotObject(value) {
+  return !isObject(value);
+}
+function isEmptyString(value) {
+  return isString(value) && value.trim() === '';
+}
+
+function isNotEmptyString(value) {
+  return !isEmptyString(value);
+}
+
+function isNullOrUndefined(value) {
+  return isNull(value) || isUndefined(value);
+}
+
+function isNotNullOrUndefined(value) {
+  return !isNullOrUndefined(value);
+}
+
+function isBoolean(value) {
+  return typeof value === 'boolean';
+}
+
+function getType(value) {
+  if (isNull(value)) return 'null';
+  if (isArray(value)) return 'array';
+
+  return typeof value;
+}
+
 export {
-  isString,
-  isFunction,
-  isObject,
-  isNotString,
-  isNotFunction,
-  isNotObject,
   isArray,
-  isNotArray
+  isNotArray,
+  isEmptyArray,
+  isNotEmptyArray,
+  isFunction,
+  isString,
+  isNotString,
+  isNull,
+  isNotNull,
+  isUndefined,
+  isObject,
+  isNotObject,
+  isEmptyString,
+  isNotEmptyString,
+  isNullOrUndefined,
+  isNotNullOrUndefined,
+  isBoolean,
+  getType
 };
 
 export default {
-  isString,
-  isFunction,
-  isObject,
-  isNotString,
-  isNotFunction,
-  isNotObject,
   isArray,
-  isNotArray
+  isNotArray,
+  isEmptyArray,
+  isNotEmptyArray,
+  isFunction,
+  isString,
+  isNotString,
+  isNull,
+  isNotNull,
+  isUndefined,
+  isObject,
+  isNotObject,
+  isEmptyString,
+  isNotEmptyString,
+  isNullOrUndefined,
+  isNotNullOrUndefined,
+  isBoolean,
+  getType
 };
 
